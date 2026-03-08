e386bb8be36a1d155b26b74a37b6d9f3e77bcf1a
Sourav Mondal <souravmondal@Souravs-MacBook-Air.local>
2026-03-08 17:42:26 +0530

    Added 2 questions

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
